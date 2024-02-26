13deabcb83f92609238ec0aad0fb5d72bca81118
Bruna Barbosa <121641582+404-blue@users.noreply.github.com>
2024-02-26 19:41:27 +0000

    new file:   "1 - 5 - Lista de exerc\303\255cios do aluno/Atv07.java" 	new file:   "1 - 5 - Lista de exerc\303\255cios do aluno/Atv08.java"

---
+++ 1 - 5 - Lista de exercícios do aluno/Atv08.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Atv08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float nota1, nota2, nota3, mediaFinal;
+
+        System.out.println("Digite a primeira nota:");
+        nota1 = scanner.nextFloat();
+
+        System.out.println("Digite a segunda nota:");
+        nota2 = scanner.nextFloat();
+
+        System.out.println("");
+    }
+}
